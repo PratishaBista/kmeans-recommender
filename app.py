9f119951f3517cfd52d338cccd407bff10bb35b5
PratishaBista <bista.pratisha@gmail.com>
2025-06-24 11:08:29 +0545

    Apply kmeans clustering algorithm successfully

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
